37cd5788a5177568b39cd14e3c1c83adddc55579
crystalbajgai <66400444+crystalbajgai@users.noreply.github.com>
2020-09-13 11:35:17 +0545

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en-us">
+		<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta name="description" content="">
+		<title>About Us</title>
+			<?php include("head.php"); ?>
+	</head>
+<script>
+	function copyNow(element) {
+  var copyText = document.getElementById(element);
+  copyText.select();
+  document.execCommand("copy");
+}
+</script>
+<style>
+body {
+	font-family: 'Fira Sans',sans-serif;
+}
+</style>
+		<body>
+<?php include("analytics.php"); ?>
+
+<?php include("header.php"); ?>
+
+<main style='margin: 0 auto;'>
+
+<div class='bodyContainer'>
+<p>Welcome to Free Youtube Thumbnail Downloader, your number one source for youtube thumbnail downloading. We're dedicated to providing you the very best of service.
+Founded in 2020 by FYTD, Free Youtube Thumbnail Downloader has come a long way from its beginnings. 
+We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.
+Sincerely,
+FYTD</p>
+</div>
+
+
+<script type="text/javascript" src="/ytg.js">
+</script>
+</main>
+
+<?php include("footer.php"); ?>
+
+
+    </body>
+        
+
+</html>
